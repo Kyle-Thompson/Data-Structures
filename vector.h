487bacf31dd1a88ddf9cf8c56c6c9100bd96fdf0
Kyle <kyle.thompson228@gmail.com>
2016-05-26 18:15:37 -0400

    A very early skeleton of vector.

--- vector.h
+++ vector.h
@@ -1,13 +1,30 @@
-//
-//  vector.h
-//  Data Structures
-//
-//  Created by Keltek on 2016-05-23.
-//  Copyright © 2016 Keltek. All rights reserved.
-//
-
 #ifndef vector_h
 #define vector_h
 
+template <class T, class Alloc = std::allocator<T>>
+class vector {
+    
+/* Type definitions */
+public:
+    typedef std::size_t size_type;
+    typedef T           value_type;
+    typedef T*          pointer;
+    typedef const T*    const_ptr;
+    typedef T&          reference;
+    typedef T&&         rvalue_ref;
+    typedef const T&    const_ref;
+    
+/* Iterators */
+    
+    
+/* Data members */
+    T* _data;
+    size_type _size = 0;
+    
+    
+/* Member functions */
+    
+    
+};
 
 #endif /* vector_h */
